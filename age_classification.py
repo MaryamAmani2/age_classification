63d96513384185805f7d894e6fad527ce50fa8d6
Maryam Amani <maryamamaniijooshin@gmail.com>
2025-09-14 21:47:56 +0330

    Add files via upload

---
+++ age_classification.py
@@ -0,0 +1,10 @@
+age = int(input("please enter the age:"))
+
+if 1 <= age <=10: 
+    print("baby") 
+if 10 < age <= 20:
+    print("teen")  
+if 20 < age <= 30:
+    print("young") 
+if age > 30:
+    print("old")       
